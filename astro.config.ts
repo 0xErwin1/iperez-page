6f3e72f42b7f203977e0be67096960c400aebff2
Ignacio Perez <ignacio.perez2123@protonmail.com>
2025-02-22 11:39:48 -0300

    feat: Added integration with Cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-  }
+  },
+  site: "https://iperez.dev",
+  output: "server",
+  adapter: cloudflare()
 });
-
