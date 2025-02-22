7277295eb23fd0d4708979cde5cb1e76d47496e4
Ignacio Perez <ignacio.perez2123@protonmail.com>
2025-02-22 09:00:39 -0300

    feat: Skills and About section

---
+++ src/enums/icons.enum.ts
@@ -0,0 +1,88 @@
+export enum Icons {
+  JAVASCRIPT = 'nf-md-language_javascript',
+  TYPESCRIPT = 'nf-md-language_typescript',
+  PYTHON = 'nf-md-language_python',
+  RUST = 'nf-md-language_rust',
+  VUE = 'nf-md-vuejs',
+  SHELL = 'nf-cod-terminal_bash',
+  JAVA = 'nf-md-language_java',
+  DOCKER = 'nf-md-docker',
+  SQL = 'nf-md-database',
+  LUA = 'nf-md-language_lua',
+  C = 'nf-custom-c',
+  'C++' = 'nf-custom-cpp',
+
+  LINUX = 'nf-cod-terminal_linux',
+  ARCH_LINUX = 'nf-linux-archlinux',
+  WINDOWS = 'nf-md-microsoft_windows',
+
+  MY_SQL = 'nf-dev-mysql',
+  POSTGRE_SQL = 'nf-dev-postgresql',
+  MONGO_DB = 'nf-dev-mongodb',
+  REDIS = 'nf-dev-redis',
+
+  AWS = 'nf-md-aws',
+  GIT = 'nf-md-git',
+  GITHUB = 'nf-md-github',
+  GITLAB = 'nf-seti-gitlab',
+  LINKEDIN = 'nf-md-linkedin',
+  NODE = 'nf-dev-nodejs_small',
+  EXPRESS = 'nf-dev-nodejs_small',
+  REACT = 'nf-md-react',
+  SPRING = 'nf-seti-spring',
+  ORANGE_PI = 'nf-md-raspberry_pi',
+  RASPBERRY_PI = 'nf-linux-raspberry_pi',
+
+  NPM = 'nf-dev-npm',
+  YARN = 'nf-seti-yarn',
+  CARGO = 'nf-md-language_rust',
+  NEOVIM = 'nf-custom-vim',
+  VSCODE = 'nf-md-microsoft_visual_studio_code',
+
+  WEBSITE = 'nf-md-web',
+}
+
+export enum IconsColor {
+  JAVASCRIPT = 'text-js-color',
+  TYPESCRIPT = 'text-ts-color',
+  SHELL = 'text-shell-color',
+  SQL = 'text-sql-color',
+  RUST = 'text-rust-color',
+  JAVA = 'text-java-color',
+  VUE = 'text-vue-color',
+  DOCKER = 'text-docker-color',
+  PYTHON = 'text-python-color',
+  LUA = 'text-lua-color',
+  C = 'text-c-color',
+  'C++' = 'text-cpp-color',
+
+  NODE = 'text-node-color',
+  REACT = 'text-react-color',
+  SPRING = 'text-spring-color',
+
+  MY_SQL = 'text-mysql-color',
+  POSTGRE_SQL = 'text-postgres-color',
+  MONGO_DB = 'text-mongo-color',
+  REDIS = 'text-redis-color',
+
+  AWS = 'text-aws-color',
+
+  GIT = 'text-git-color',
+  GITHUB = 'text-github-color',
+  GITLAB = 'text-gitlab-color',
+  LINKEDIN = 'text-linkedin-color',
+
+  LINUX = 'text-linux-color',
+  ARCH_LINUX = 'text-archlinux-color',
+  WINDOWS = 'text-windows-color',
+
+  ORANGE_PI = 'text-orange-color',
+  RASPBERRY_PI = 'text-raspberry-color',
+
+  NPM = 'text-npm-color',
+  YARN = 'text-yarn-color',
+  NEOVIM = 'text-vim-color',
+  VSCODE = 'text-vscode-color',
+
+  WEBSITE = 'text-website-color',
+}
